82dd55b1b183985edebbec92b91ec8e2bca6dfbf
huyit1999a <huyit1999a@gmail.com>
2022-03-04 16:24:58 +0700

    import bootstrap
    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import ListUser from "./components/ListUser";
-import Login from "./components/login";
-import DropdownList from "./components/modal";
-import { Button } from "react-bootstrap";
-import React from "react";
-
+import './App.css';
+import ListUser from './components/ListUser';
+import Login from './components/login';
+import DropdownList from './components/modal';
+import { Button } from 'react-bootstrap';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [modalShow, setModalShow] = React.useState(false);
-  return (
-    <div>
-      {/* <Login /> */}
-      <ListUser />
-    </div>
-  );
+    const [modalShow, setModalShow] = React.useState(false);
+    return (
+        <div>
+            {/* <Login /> */}
+            <ListUser />
+        </div>
+    );
 }
 
 export default App;
